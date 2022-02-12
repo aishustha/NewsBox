f4b1b4cf03498837c5129a16012777adce5d9402
Aishwarya Shrestha <aishwaryashrestha4064@gmail.com>
2022-02-12 14:31:16 -0500

    Added

--- app/src/main/java/com/example/mapd726_group3_newsbox/MainActivity.kt
+++ app/src/main/java/com/example/mapd726_group3_newsbox/MainActivity.kt
@@ -1,23 +1,20 @@
 package com.example.mapd726_group3_newsbox
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val initialBtn = findViewById<Button>(R.id.initialBtn)
-//
-//        initialBtn.setOnClickListener {
-//            val intent = Intent(this, MainActivity::class.java)
-//            startActivity(intent)
-//        }
-
+        val logoBtn = findViewById<Button>(R.id.profile_btn)
+        logoBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, SignIn::class.java)
+            startActivity(intent)
+        }
     }
+
 }
