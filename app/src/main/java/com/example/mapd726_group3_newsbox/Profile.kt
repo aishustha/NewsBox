ab4d666ead3a429088091b45c2b067fe8f4073a3
matthew maxwell <matthewmaxwell300@yahoo.com>
2022-02-14 13:13:56 -0700

    changed apikey

--- app/src/main/java/com/example/mapd726_group3_newsbox/Profile.kt
+++ app/src/main/java/com/example/mapd726_group3_newsbox/Profile.kt
@@ -1,14 +1,24 @@
 package com.example.mapd726_group3_newsbox
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.ActionBar
 import com.example.mapd726_group3_newsbox.databinding.ActivityExploreBinding
 import com.example.mapd726_group3_newsbox.databinding.ActivityProfileBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class Profile : AppCompatActivity() {
+    //database reference for reading from database
+    //private lateinit var database: DatabaseReference
+    private lateinit var database:  FirebaseFirestore
+
 
     //ViewBinding
     private lateinit var binding: ActivityProfileBinding
@@ -33,11 +43,44 @@ class Profile : AppCompatActivity() {
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
 
+        //connection for reading from database
+        //database = Firebase.database.reference
+        database= FirebaseFirestore.getInstance()
+
         //handle click, logout
         binding.logoutBtn.setOnClickListener {
             firebaseAuth.signOut()
             checkUser()
         }
+        getArticle()
+    }
+
+    private fun getArticle()
+    {
+        database.collection("Articles")
+            .whereEqualTo("category", "space")
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents: ", exception)
+            }
+//        // Create a reference to the cities collection
+//        val spaceCategory = database.collection("Articles")
+//        // Create a query against the collection.
+//        val query = spaceCategory.whereEqualTo("category", "space").get()
+//        println(query)
+
+       // database.child("Articles").child("category").equals("space")
+
+//        database.child("Articles").child("category").get().addOnSuccessListener {
+//            Log.i("firebase", "Got value ${it.}")
+//        }.addOnFailureListener{
+//            Log.e("firebase", "Error getting data", it)
+//        }
     }
 
     private fun checkUser() {
