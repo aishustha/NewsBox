4f63eef043fe9af85a3d9b8c4bcda730a5bb7c33
Aishwarya Shrestha <aishwaryashrestha4064@gmail.com>
2022-02-28 19:12:20 -0500

    Added details

--- app/src/main/java/com/example/mapd726_group3_newsbox/Profile.kt
+++ app/src/main/java/com/example/mapd726_group3_newsbox/Profile.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -71,7 +73,16 @@ class Profile : AppCompatActivity() {
 
         articlearrayList = arrayListOf<Article>()
         getDataList()
+
+        val viewBtn = findViewById<Button>(R.id.view_btn)
+        viewBtn.setOnClickListener {
+            val intent = Intent(this@Profile, Detail::class.java)
+            startActivity(intent)
+        }
+
     }
+
+
 
     //gets list from collections where category matches the search string cat
 
